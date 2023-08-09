5edbadc692c4a0acd09d1426b851121c9c8cf24c
zeozeozeo <108888572+zeozeozeo@users.noreply.github.com>
2023-08-09 17:23:40 +0500

    Copy FleX DLLs into the target directory when building
    This is kinda hacky but it works

--- nvflex-sys/build.rs
+++ nvflex-sys/build.rs
@@ -26,8 +26,11 @@ fn main() {
         .expect("unable to write bindings");
 
     // link libraries
-    // TODO: we should also copy FleX dynamic libraries to the target directory
-    link()
+    link();
+
+    // if we're on Windows, copy the DLL files into the target directory
+    #[cfg(target_os = "windows")]
+    copy_dlls_to_target_directory();
 }
 
 macro_rules! link_lib {
@@ -42,7 +45,7 @@ const VERSION: &str = "Debug";
 #[cfg(not(debug_assertions))]
 const VERSION: &str = "Release";
 
-#[cfg(all(target_os = "windows"))]
+#[cfg(target_os = "windows")]
 fn link() {
     let ptr_width = env::var("CARGO_CFG_TARGET_POINTER_WIDTH")
         .expect("CARGO_CFG_TARGET_POINTER_WIDTH enviroment variable not set");
@@ -76,7 +79,7 @@ fn link() {
     }
 }
 
-#[cfg(all(target_os = "linux"))]
+#[cfg(target_os = "linux")]
 fn link() {
     let ptr_width = env::var("CARGO_CFG_TARGET_POINTER_WIDTH")
         .expect("CARGO_CFG_TARGET_POINTER_WIDTH enviroment variable not set");
@@ -102,7 +105,7 @@ fn link() {
     }
 }
 
-#[cfg(all(target_os = "android"))]
+#[cfg(target_os = "android")]
 fn link() {
     let path = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap())
         .join("FleX")
@@ -120,5 +123,109 @@ fn link() {
 
         #[cfg(feature = "ext")]
         link_lib!("libNvFlexExt{}CUDA_aarch64", VERSION);
+    }
+}
+
+#[cfg(target_os = "windows")]
+fn copy_dlls_to_target_directory() {
+    // we need to get to the target/debug or the target/release directory
+    // we can use the `OUT_DIR` envvar which points to target/debug/build/nvflex-sys.*./out,
+    // so we need to pop the last 3 entries of the path
+    let mut out_dir =
+        PathBuf::from(std::env::var("OUT_DIR").expect("OUT_DIR enviroment variable not set"));
+    out_dir.pop(); // pop `/out`
+    out_dir.pop(); // pop `/nvflex-sys.*.`
+    out_dir.pop(); // pop `/build`
+
+    let ptr_width = env::var("CARGO_CFG_TARGET_POINTER_WIDTH")
+        .expect("CARGO_CFG_TARGET_POINTER_WIDTH enviroment variable not set");
+
+    #[cfg(debug_assertions)]
+    const DEBUG_BUILD: bool = true;
+
+    #[cfg(not(debug_assertions))]
+    const DEBUG_BUILD: bool = false;
+
+    let flex_libs: Vec<&str> = if ptr_width == "64" {
+        // win64
+        vec![
+            "amd_ags_x64.dll",
+            "cudart64_92.dll",
+            "GFSDK_Aftermath_Lib.x64.dll",
+            if DEBUG_BUILD {
+                "NvFlexDebugCUDA_x64.dll"
+            } else {
+                "NvFlexReleaseCUDA_x64.dll"
+            },
+            if DEBUG_BUILD {
+                "NvFlexDebugD3D_x64.dll"
+            } else {
+                "NvFlexReleaseD3D_x64.dll"
+            },
+            if DEBUG_BUILD {
+                "NvFlexDeviceDebug_x64.dll"
+            } else {
+                "NvFlexDeviceRelease_x64.dll"
+            },
+            if DEBUG_BUILD {
+                "NvFlexExtDebugCUDA_x64.dll"
+            } else {
+                "NvFlexExtReleaseCUDA_x64.dll"
+            },
+            if DEBUG_BUILD {
+                "NvFlexExtDebugD3D_x64.dll"
+            } else {
+                "NvFlexExtReleaseD3D_x64.dll"
+            },
+            "nvToolsExt64_1.dll",
+        ]
+    } else {
+        // win32
+        vec![
+            "amd_ags_x86.dll",
+            "cudart32_92.dll",
+            "GFSDK_Aftermath_Lib.x86.dll",
+            if DEBUG_BUILD {
+                "NvFlexDebugCUDA_x86.dll"
+            } else {
+                "NvFlexReleaseCUDA_x86.dll"
+            },
+            if DEBUG_BUILD {
+                "NvFlexDebugD3D_x86.dll"
+            } else {
+                "NvFlexReleaseD3D_x86.dll"
+            },
+            if DEBUG_BUILD {
+                "NvFlexDeviceDebug_x86.dll"
+            } else {
+                "NvFlexDeviceRelease_x86.dll"
+            },
+            if DEBUG_BUILD {
+                "NvFlexExtDebugCUDA_x86.dll"
+            } else {
+                "NvFlexExtReleaseCUDA_x86.dll"
+            },
+            if DEBUG_BUILD {
+                "NvFlexExtDebugD3D_x86.dll"
+            } else {
+                "NvFlexExtReleaseD3D_x86.dll"
+            },
+            "nvToolsExt32_1.dll",
+        ]
+    };
+
+    for lib in flex_libs {
+        let mut dest_path = out_dir.clone();
+        dest_path.push(lib);
+        if dest_path.exists() {
+            continue; // no need to copy this dll, it is already present
+        }
+
+        let mut src_path =
+            PathBuf::from("FleX/bin/win".to_string() + if ptr_width == "64" { "64" } else { "32" });
+        src_path.push(lib);
+
+        // copy the library dll
+        let _ = std::fs::copy(src_path, dest_path);
     }
 }
